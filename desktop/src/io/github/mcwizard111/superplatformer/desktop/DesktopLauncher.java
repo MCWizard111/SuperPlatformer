95ccf3520a8bdbcdab85771570e8ecd22146fceb
MCWizard111 <mcwizard111@live.com>
2014-09-04 01:29:11 -0400

    Initial Commit (Without Readme)

---
+++ desktop/src/io/github/mcwizard111/superplatformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.mcwizard111.superplatformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.mcwizard111.superplatformer.SuperPlatformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperPlatformer(), config);
+	}
+}
